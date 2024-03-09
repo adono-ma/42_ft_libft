f3d4eabb6c0f085c2e3815bac01c560c6adebc33
adono-ma <157633210+adono-ma@users.noreply.github.com>
2024-03-09 22:17:57 +0100

    new functions
    approx. 10 left

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -1,33 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adono-ma <adono-ma@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/09 21:54:07 by adono-ma          #+#    #+#             */
-/*   Updated: 2024/03/09 22:14:48 by adono-ma         ###   ########.fr       */
+/*   Created: 2024/03/09 22:03:00 by adono-ma          #+#    #+#             */
+/*   Updated: 2024/03/09 22:14:49 by adono-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include    "libft.h"
 
-size_t   ft_strlcpy(char    *dest, const char *src, size_t  n)
+size_t   ft_strlcat(char    *dest, const char *src, size_t  n)
 {
+   size_t   destn;
+   size_t   srcn;
    size_t   i;
-   size_t   count;
-   char *cpy;
    
+   destn = ft_strlen(dest);
+   srcn = ft_strlen(src);
    i = 0;
-   count = ft_strlen(src);
-   cpy = (char  *)src;
-   if (n == 0)
-        return(count);
-   while (cpy[i] != '\0' && i < (n - 1))
+   if (dest == 0 && n == 0)
+    return (n + srcn);
+   if (n > destn)
    {
-    dest[i] = cpy[i];
-    i++;
+    while (*src != 0 && i < (n - destn - 1))
+    {
+        dest[destn + i] = *src;
+        src++;
+        i++;
+    }
+    dest[destn + i] = 0;
+   return (destn + srcn);
    }
-   dest[i] = '\0';
-   return (count);
+   else
+    return (n + srcn);
 }
