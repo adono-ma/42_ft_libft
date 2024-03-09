7e52b3ee849f509649cf94d046ed0848d857bcc9
Ana Belen Dono Matellano <adono-ma@c2r3s1.42madrid.com>
2024-03-09 16:38:34 +0100

    Actualizacion 09 de Marzo del 2024

---
+++ Libft/libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adono-ma <adono-ma@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 18:55:40 by adono-ma          #+#    #+#             */
+/*   Updated: 2024/03/09 16:25:16 by adono-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+#define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_isalpha(int i);
+int ft_strlen(const char *str);
+int ft_isascii(int i);
+int ft_isdigit(int i);
+int ft_isprint(int i);
+int ft_strlen(const char *str);
+
+#endif
